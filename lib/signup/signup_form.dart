52be353d2334ca6ca1fe5dfc9096c85556f35a7f
Pannathorn <pttv48@gmail.com>
2025-03-08 21:07:05 +0700

    add services

--- lib/signup/signup_form.dart
+++ lib/signup/signup_form.dart
@@ -3,6 +3,7 @@ import 'package:flutter_svg/svg.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:mobile_project/components/button.dart';
 import 'package:mobile_project/components/input_feild.dart';
+import 'package:mobile_project/services/validator.dart';
 
 class SignUpForm extends StatefulWidget {
   const SignUpForm({super.key});
@@ -23,7 +24,7 @@ class _SignUpFormState extends State<SignUpForm> {
               label: "Email",
               keyboardType: TextInputType.emailAddress,
               hintText: "Enter your email",
-              validator: validateEmail,
+              validator: Validator.validateEmail,
             ),
             const SizedBox(
               height: 15,
@@ -92,22 +93,4 @@ class _SignUpFormState extends State<SignUpForm> {
           ],
         ));
   }
-}
-
-String? validateEmail(String? value) {
-  if (value == null || value.isEmpty) {
-    return 'Please enter your email';
-  }
-  const pattern = r"(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'"
-      r'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-'
-      r'\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*'
-      r'[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4]'
-      r'[0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9]'
-      r'[0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\'
-      r'x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])';
-  final regex = RegExp(pattern);
-
-  return value.isNotEmpty && !regex.hasMatch(value)
-      ? 'Enter a valid email address'
-      : null;
 }
